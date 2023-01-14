0b680f9ce47a71c6fce39171ab820e6ae7887a6d
FilmBee <92419399+FilmBee@users.noreply.github.com>
2023-01-14 21:22:34 +0600

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,5 +1,5 @@
 echo ">> FETCHING UPSTREAM..."
-git clone https://github.com/FilmBee/MusicPlayer /MusicPlayer
+git clone https://github.com/FilmBee/MusicPlayer /MusicPlaye
 echo ">> INSTALLING REQUIREMENTS..."
 cd /MusicPlayer
 pip3 install -U -r requirements.txt
